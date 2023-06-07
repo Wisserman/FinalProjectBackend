0565f951aed34fbff6e255d45b27a286ca2c5159
EMMANUEL COOKE <76752588+emmanuelcooke@users.noreply.github.com>
2023-06-07 11:44:44 -0600

    Updated ItemController.java

--- controller/ItemController.java
+++ controller/ItemController.java
@@ -21,4 +21,47 @@ public class ItemController {
     public Iterable<Item> getItems(){
         return itemRepository.findAll();
     }
+    
+    @PostMapping
+    public ResponseEntity<Item> createItem(@RequestBody ItemDto itemDto) {
+        //convert the itemDto to item
+        Item newItem = convertToItem(itemDto);
+
+        return Response.Entity.status(HttpStatus.Created).body(newItem);
+    }
+    // Helper method to convert ItemDto to Item
+    private Item convertToItem(ItemDto itemDto) {
+        Item item = new Item();
+        item.setName(itemDto.getName());
+        item.setDescription(itemDto.getDescription());
+
+        return item;
+    }
+
+    @PostMapping
+    public Item save( @RequestBody ItemDto itemDto )
+    {
+        return itemService.save( new Item( itemDto ) );
+    }
+
+    @GetMapping("/{id}")
+    public Item findItemById( @PathVariable Integer id ){
+        return itemService.findById( id );
+    }
+
+    @PutMapping( "/{id}" )
+    public Item update( @RequestBody ItemDto itemDto, @PathVariable Integer id )
+    {
+        Item item = itemService.findById( id );
+        item.setName( itemDto.getName() );
+        item.setDescription( itemDto.getDescription() );
+        item.setImageUrl( itemDto.getImageUrl() );
+        return itemService.save( item );
+    }
+
+    @DeleteMapping( "/{id}" )
+    public void delete( @PathVariable Integer id )
+    {
+        itemService.delete( id );
+    }
 }
